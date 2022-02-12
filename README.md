4aa33c31bf4422fb01f1b9a3e0d8bd2fe093f7b0
Jan Luhmann <jan.luhmann@exb.de>
2022-02-13 00:35:15 +0100

    Implement SampleExtractor. Create poetry packaging.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # drumbot_roy
+
+## Training data
+
+### Lakh MIDI Dataset v0.1
+"LMD-full" available at https://colinraffel.com/projects/lmd/
+
+### Groove MIDI Dataset
+"groove-v1.0.0-midionly.zip" available at 
+https://magenta.tensorflow.org/datasets/groove#download
---
+++ src/drumbot_roy/training/sample_extractor.py
@@ -0,0 +1,59 @@
+import io
+from typing import List, Optional
+
+import miditoolkit.midi.parser as midi_parser
+from miditoolkit.midi.parser import MidiFile
+from miditoolkit.midi.containers import TimeSignature
+
+
+class SampleExtractor:
+    def __init__(
+            self,
+            bars_per_sample: int = 2
+    ):
+        self.bars_per_sample = bars_per_sample
+        pass
+
+    def extract_samples(
+            self,
+            midi_obj: MidiFile,
+    ):
+        samples = []
+
+        if len(midi_obj.time_signature_changes) == 0:
+            return samples
+
+        start = 0
+        end = 0
+
+        while start < midi_obj.max_tick:
+            for _ in range(self.bars_per_sample):
+                time_sig = self.get_time_signature_at_tick(
+                    tick=end,
+                    time_signatures=midi_obj.time_signature_changes
+                )
+                fourths_per_bar = time_sig.numerator / (time_sig.denominator / 4)
+                end += fourths_per_bar * midi_obj.ticks_per_beat
+
+            stream = io.BytesIO()
+            midi_obj.dump(file=stream, segment=(start, end))
+            stream.seek(0)
+            sample = midi_parser.MidiFile(file=stream)
+            samples.append(sample)
+
+            start = end
+
+        return samples
+
+    def get_time_signature_at_tick(
+            self,
+            tick: int,
+            time_signatures: List[TimeSignature]
+    ) -> Optional[TimeSignature]:
+        if len(time_signatures) == 0:
+            return
+        time_sig_at_tick = time_signatures[0]
+        for time_sig in time_signatures[1:]:
+            if tick >= time_sig.time:
+                time_sig_at_tick = time_sig
+        return time_sig_at_tick

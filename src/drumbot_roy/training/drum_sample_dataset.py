b0f46c4cc17c340d417f9fff39360abe05468d3e
Jan Luhmann <jan.luhmann@exb.de>
2022-03-16 09:28:29 +0100

    Add first draft of DrumSampleDataset

---
+++ src/drumbot_roy/training/drum_sample_dataset.py
@@ -0,0 +1,144 @@
+import logging
+import random
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Optional, Union
+
+from miditok import Octuple
+from torch.utils.data import Dataset
+from miditoolkit.midi import parser as midi_parser
+from tqdm.auto import tqdm
+
+from src.drumbot_roy.training.sample_extractor import SampleExtractor
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class RawSample:
+    file_path: Path
+    start: int
+    end: int
+    midi: midi_parser.MidiFile
+
+
+@dataclass
+class TokenizedSample:
+    file_path: Path
+    start: int
+    end: int
+    tokens: List[int]
+
+
+@dataclass
+class TokenizedSamplePair:
+    sample_a: TokenizedSample
+    sample_b: TokenizedSample
+    distance: float
+
+
+class DrumSampleDataset(Dataset):
+    def __init__(
+        self,
+        input_dir: Path,
+        bars_per_sample: int,
+        paired: bool = False,
+        filter_out_empty_samples: bool = True,
+        shuffle_files: bool = True,
+        shuffle_seed: int = 42,
+    ) -> None:
+        self.midi_file_paths = list(input_dir.rglob("*.mid")) + list(
+            input_dir.rglob("*.midi")
+        )
+
+        random.seed(shuffle_seed)
+        if shuffle_files:
+            random.shuffle(self.midi_file_paths)
+
+        self.paired = paired
+        self.filter_out_empty_samples = filter_out_empty_samples
+
+        self.samples: Optional[List[RawSample]] = None
+        self.sample_extractor = SampleExtractor(
+            bars_per_sample=bars_per_sample
+        )
+
+        self.tokenizer = Octuple(nb_velocities=8)
+
+    def prepare_samples(self):
+        for midi_file_path in tqdm(self.midi_file_paths):
+            for sample in self.sample_extractor.extract_samples(
+                midi_file_path=midi_file_path
+            ):
+                number_of_notes = self.sample_extractor.get_number_of_notes(
+                    midi_obj=sample.midi
+                )
+                if self.filter_out_empty_samples and number_of_notes == 0:
+                    continue
+
+                self.samples.append(sample)
+
+    def __len__(self) -> Optional[int]:
+        if self.samples is None:
+            logger.warning(
+                "Please run DrumSampleDataset.prepare_samples first "
+                "to preprocess the samples."
+            )
+        elif not self.paired:
+            return len(self.samples)
+        else:
+            # For each sample we generate one same-song pair
+            # and one different-song pair
+            return len(self.samples) * 2
+
+    def __getitem__(self, item) -> Union[TokenizedSample, TokenizedSamplePair]:
+        if self.samples is None:
+            logger.warning(
+                "Please run DrumSampleDataset.prepare_samples first "
+                "to preprocess the samples."
+            )
+            return
+
+        if not self.paired:
+            sample = self.samples[item]
+            return TokenizedSample(
+                file_path=sample.file_path,
+                start=sample.start,
+                end=sample.end,
+                tokens=self.tokenizer.midi_to_tokens(sample.midi)[0],
+            )
+        else:
+            sample_a = self.samples[item // 2]
+            distance = 1
+            if item % 2 == 0:
+                sample_b_idx = (item // 2) + 1
+                if (
+                    sample_b_idx >= len(self)
+                    or self.samples[sample_b_idx].file_path
+                    != sample_a.file_path
+                ):
+                    sample_b_idx = (item // 2) - 1
+
+                sample_b = self.samples[sample_b_idx]
+                if sample_a.file_path == sample_b.file_path:
+                    distance = 0
+            else:
+                sample_b = self.samples[
+                    int((item + (len(self) / 2)) % len(self))
+                ]
+
+            return TokenizedSamplePair(
+                sample_a=TokenizedSample(
+                    file_path=sample_a.file_path,
+                    start=sample_a.start,
+                    end=sample_a.end,
+                    tokens=self.tokenizer.midi_to_tokens(sample_a.midi),
+                ),
+                sample_b=TokenizedSample(
+                    file_path=sample_b.file_path,
+                    start=sample_b.start,
+                    end=sample_b.end,
+                    tokens=self.tokenizer.midi_to_tokens(sample_b.midi),
+                ),
+                distance=distance,
+            )
--- src/drumbot_roy/training/sample_extractor.py
+++ src/drumbot_roy/training/sample_extractor.py
@@ -1,9 +1,15 @@
 import io
+import logging
+from pathlib import Path
 from typing import List, Optional, Generator
 
 import miditoolkit.midi.parser as midi_parser
 from miditoolkit.midi.parser import MidiFile
 from miditoolkit.midi.containers import TimeSignature
+
+from src.drumbot_roy.training.drum_sample_dataset import RawSample
+
+logger = logging.getLogger(__name__)
 
 
 class SampleExtractor:
@@ -11,9 +17,15 @@ class SampleExtractor:
         self.bars_per_sample = bars_per_sample
 
     def extract_samples(
-        self,
-        midi_obj: MidiFile,
-    ) -> Generator[MidiFile, None, None]:
+        self, midi_file_path: Path, midi_obj: Optional[MidiFile] = None
+    ) -> Generator[RawSample, None, None]:
+        if midi_obj is None:
+            try:
+                midi_obj = MidiFile(filename=midi_file_path)
+            except Exception as exception:
+                logger.warning(exception)
+                return None
+
         if len(midi_obj.time_signature_changes) == 0:
             return None
 
@@ -25,14 +37,18 @@ class SampleExtractor:
                 time_sig = self.get_time_signature_at_tick(
                     tick=end, time_signatures=midi_obj.time_signature_changes
                 )
-                fourths_per_bar = time_sig.numerator / (time_sig.denominator / 4)
+                fourths_per_bar = time_sig.numerator / (
+                    time_sig.denominator / 4
+                )
                 end += fourths_per_bar * midi_obj.ticks_per_beat
 
             stream = io.BytesIO()
             midi_obj.dump(file=stream, segment=(start, end))
             stream.seek(0)
             sample = midi_parser.MidiFile(file=stream)
-            yield sample
+            yield RawSample(
+                file_path=midi_file_path, start=start, end=end, midi=sample
+            )
 
             start = end
 
@@ -45,3 +61,12 @@ class SampleExtractor:
         for time_sig in time_signatures[1:]:
             if tick >= time_sig.time:
                 return time_sig_at_tick
+
+    def get_number_of_notes(self, midi_obj: MidiFile) -> int:
+        return len(
+            [
+                note
+                for instrument in midi_obj.instruments
+                for note in instrument
+            ]
+        )
